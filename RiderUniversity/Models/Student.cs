53fdcde044aede4b0b3e5616a77749922c673358
ilham <m.ilham@id.jmtech.co.jp>
2015-10-16 17:51:12 +0700

    First commit

---
+++ RiderUniversity/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace RiderUniversity.Models
+{
+    public class Student : Person
+    {
+        [DataType(DataType.Date)]
+        [Display(Name = "Enrollment Date")]
+        public DateTime EnrollmentDate { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
